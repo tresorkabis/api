a780ca85a3096230eb057a54485b9ee3040c9d8a
Tresor <tresorkabis@gmail.com>
2025-01-28 16:47:39 +0100

    ann

--- src/main/java/cd/esforca/api/controller/ProduitController.java
+++ src/main/java/cd/esforca/api/controller/ProduitController.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import cd.esforca.api.model.Produit;
 import cd.esforca.api.service.ProduitService;
 
-
+@RestController
 public class ProduitController {
 
     @Autowired
